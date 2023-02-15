72edd1f19e7a4aaaf9625abb7eb65afc8f4994fb
cliffordru <clifford.gray@gmail.com>
2023-02-14 22:32:16 -0500

    Add authentication support in swagger - allows for entering a custom header value in the swagger UI - https://localhost:7048/swagger/index.html

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ApiKeyAuthentication.Authentication;
+using Microsoft.OpenApi.Models;
 
 namespace ApiKeyAuthentication
 {
@@ -11,7 +12,31 @@ namespace ApiKeyAuthentication
             builder.Services.AddAuthorization();
             builder.Services.AddControllers(/*x => x.Filters.Add<ApiKeyAuthFilter>()*/);
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    Description = "The API Key to access the API",
+                    Type = SecuritySchemeType.ApiKey,
+                    Name = "x-api-key",
+                    In = ParameterLocation.Header,
+                    Scheme = "ApiKeyScheme"
+                });
+                var scheme = new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "ApiKey"
+                    },
+                    In = ParameterLocation.Header
+                };
+                var requirement = new OpenApiSecurityRequirement
+                {
+                    {scheme, new List<string>() }
+                };
+                c.AddSecurityRequirement(requirement);
+            });
 
             builder.Services.AddScoped<ApiKeyAuthFilter>();
 
